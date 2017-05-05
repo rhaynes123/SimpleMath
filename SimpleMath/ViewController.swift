83c697993962c65c0382f111d0f895988c5d02ab
Richard <rahayn@yahoo.com>
2017-05-05 17:14:41 -0400

    Initial Commit

---
+++ SimpleMath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleMath
+//
+//  Created by richard Haynes on 5/5/17.
+//  Copyright © 2017 Me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
