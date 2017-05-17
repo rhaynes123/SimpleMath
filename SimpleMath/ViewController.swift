7c4bf4db638b8fd4699804c221803065ffe578c5
Richard <rahayn@yahoo.com>
2017-05-17 15:37:36 -0400

    Simple Math Calculator
    This is a basic calculator project I’m creating

--- SimpleMath/ViewController.swift
+++ SimpleMath/ViewController.swift
@@ -9,7 +9,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var displayedNum: Double = 0;
+    var previousNum: Double = 0;
+    var doSomeMath = false
+    var userOperation = 0;
+    @IBOutlet weak var Label: UILabel!
+   
+    @IBAction func numbers(_ sender: UIButton)
+    {
+        if doSomeMath == true
+        {
+            Label.text = String(sender.tag-1)
+            displayedNum = Double(Label.text!)!
+            doSomeMath = false
+        }
+        else
+        {
+            Label.text = Label.text! + String(sender.tag-1)
+            displayedNum = Double(Label.text!)!
+        }
+    }
 
+    @IBAction func buttons(_ sender: UIButton)
+    {
+       if Label.text != "" && sender.tag != 16 && sender.tag != 15
+        {
+            
+            previousNum = Double(Label.text!)!
+            if sender.tag == 11 //divide
+            {
+                Label.text = "/";
+            }
+            else if sender.tag == 12// multiply
+            {
+                Label.text = "x";
+            }
+            else if sender.tag == 13//minus
+            {
+                Label.text = "-";
+            }
+            else if sender.tag == 14//addition
+            {
+                Label.text = "+";
+            }
+            userOperation = sender.tag;
+            doSomeMath = true;
+        }
+        else if  sender.tag == 15
+        {
+            if userOperation == 11
+            {
+                Label.text = String (previousNum / displayedNum)
+            }
+            if userOperation == 13
+            {
+                Label.text = String (previousNum - displayedNum)
+            }
+
+            if userOperation == 14
+            {
+                Label.text = String (previousNum + displayedNum)
+            }
+
+            if userOperation == 12
+            {
+                Label.text = String (previousNum * displayedNum)
+            }
+
+        }
+        else if sender.tag == 16
+        {
+            Label.text = ""
+            previousNum = 0;
+            displayedNum = 0
+            userOperation = 0;
+        }
+       
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
